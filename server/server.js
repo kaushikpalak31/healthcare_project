e2c8e4ea34e7787d47fa96a1a0c75cf343c26370
palakkaushik <kaushik.palak05@gmail.com>
2024-10-21 15:12:23 +0530

    gitignore created

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
-console.log("test");
+const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandler);
+//routes below
+app.get('./'(req,res)=>{
+    res.send("working")
+})
+
+
 
