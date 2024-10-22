ef314282d2420f848a0f5d6477e76fa3a54aedab
palakkaushik <kaushik.palak05@gmail.com>
2024-10-22 15:49:00 +0530

    updated db connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
+//framework configuration
 const express = require('express');
 const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
-const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");     //handles error
+const cors = require("cors");        //puts restriction for outside source
+ 
+//env file config
+const dotenv = require("dotenv");
+dotenv.config(); //enabling the env file
 
 connectDb();
 const app = express();
@@ -10,6 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+//error handling middleware
 app.use(errorHandler);
 //routes below
 app.get('/',(req,res)=>{
@@ -19,5 +25,6 @@ app.listen(port,()=>{
     console.log("server working on port5000");
 })
 
+module.exports = connectDb;
 
 
